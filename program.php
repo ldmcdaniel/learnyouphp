ba85dcd780bf865ac1088a7f36b747feb6a70197
Lucas McDaniel <lucas.mcdaniel@bernardhealth.com>
2016-02-09 10:25:59 -0600

    Final answer

--- program.php
+++ program.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Hello World';
+require_once __DIR__ . '/mymodule.php';
+
+$myFilter = new DirectoryFilter();
+
+$returnedArray = $myFilter->filterFiles($argv[1], $argv[2]);
+
+print implode(PHP_EOL, $returnedArray) . PHP_EOL;
